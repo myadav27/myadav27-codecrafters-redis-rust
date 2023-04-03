596cdbf33f63c21463f0a86ef0da13f6972ec65a
Manoj <myadav@microsoft.com>
2023-04-03 09:02:48 -0400

    update tcp listener

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io::{Read, Write};
+use std::net::TcpListener;
+
+fn main() {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    println!("Logs from your program will appear here!");
+
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    #[allow(clippy::unused_io_amount)]
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                println!("accepted new connection");
+                let mut buf = [0; 512];
+                loop {
+                    let bytes_read = stream.read(&mut buf).unwrap();
+                    if bytes_read == 0 {
+                        println!("client closed the connection");
+                        break;
+                    }
+
+                    stream.write("+PONG\r\n".as_bytes()).unwrap();
+                }
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
+}
