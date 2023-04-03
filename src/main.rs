93d5f731be60a37d40b331186ef9fce1583e1b75
Manoj <myadav@microsoft.com>
2023-04-03 08:53:03 -0400

    #3

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,29 @@
-use std::{io::{BufRead, BufReader, Write}, net::TcpListener};
+use std::io::{Read, Write};
+use std::net::TcpListener;
+// use std::{io::Write, net::TcpListener};
+
+use tokio::stream;
 
 fn main() {
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
+    #[allow(clippy::unused_io_amount)]
     for stream in listener.incoming() {
         match stream {
             Ok(mut stream) => {
-                let reader = BufReader::new(&stream);
-                for line in reader.lines() {
-                    match line {
-                        Ok(line) => {
-                            if line == "PING\r\n" {
-                                let response = "+PONG\r\n".as_bytes();
-                                stream.write_all(&response).unwrap();
-                            }
-                        }
-                        Err(e) => {
-                            println!("error reading line: {}", e);
-                            break;
-                        }
+                println!("accepted new connection");
+                let mut buf = [0; 512];
+                loop {
+                    let bytes_read = stream.read(&mut buf).unwrap();
+                    if bytes_read == 0 {
+                        println!("client closed the connection");
+                        break;
                     }
+
+                    stream.write("+PONG\r\n".as_bytes()).unwrap();
                 }
             }
             Err(e) => {
